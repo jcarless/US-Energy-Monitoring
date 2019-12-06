cdefc85dc82710d6ddf25343d8ccf74a0b60486f
Jerome Carless <jerome@A8435.lan1>
2019-12-05 20:25:18 -0500

    gzipping data

--- airflow/dags/operators/us_traffic_incident_details.py
+++ airflow/dags/operators/us_traffic_incident_details.py
@@ -1,6 +1,9 @@
 import logging
 import requests
 from datetime import datetime
+import io
+import gzip
+import json
 
 from airflow.hooks.S3_hook import S3Hook
 from airflow.hooks.base_hook import BaseHook
@@ -12,35 +15,54 @@ URL = f"https://api.tomtom.com/traffic/services"
 
 def load_traffic_incident_details(bounding_box=None, api_key=None, bucket_name='real-time-traffic', s3_connection='s3_connection', **kwargs):
     if api_key is None:
-        print("Loading API Key")
+        print("Loading API Key...")
         api_key = BaseHook.get_connection("tomtom_api").password
 
     if bounding_box is None:
-        print("Loading Bound Box")
+        print("Loading Bound Box...")
         bounding_box = Variable.get("usa_bounding_box")
 
     traffic_incident_details_data = get_traffic_details(bounding_box, api_key)
     save_traffic_incident_details(traffic_incident_details_data, bucket_name, s3_connection)
-    return
 
 
-def save_traffic_incident_details(traffic_incident_details_data, bucket_name, s3_connection):
-    print("Saving to s3")
-    s3 = S3Hook(aws_conn_id=s3_connection)
-    date_time = datetime.utcnow().replace(microsecond=0).isoformat()
+def save_traffic_incident_details(data, bucket_name, s3_connection):
+    print("Uploading to s3...")
 
-    key = f'traffic_details/raw/{date_time}'
+    try:
+        s3 = S3Hook(aws_conn_id=s3_connection)
+        date_time = datetime.utcnow().replace(microsecond=0).isoformat()
 
-    return s3.load_string(traffic_incident_details_data,
-                          key=key,
-                          bucket_name=bucket_name,
-                          encoding='utf-8')
+        key = f'traffic_details/raw/{date_time}.json.gz'
+
+        s3.load_bytes(zip_data(data),
+                      key=key,
+                      bucket_name=bucket_name)
+
+    except BaseException as e:
+        print("Failed to upload to s3!")
+        raise e
+
+
+def zip_data(data):
+    print("Zipping incedent details...")
+    try:
+        gz_body = io.BytesIO()
+        gz = gzip.GzipFile(None, 'wb', 9, gz_body)
+        gz.write(data.encode('utf-8'))
+        gz.close()
+        return gz_body.getvalue()
+
+    except BaseException as e:
+        print("Zip failed!")
+        raise e
 
 
 def get_traffic_details(bounding_box, api_key):
 
+    print("Getting incedent details...")
+
     try:
-        print("Getting incedent details")
         PARAMS = {
             'projection': 'EPSG900913',
             'key': api_key,
@@ -59,15 +81,11 @@ def get_traffic_details(bounding_box, api_key):
 
         return data
     except BaseException as e:
+        print("Failed to extract incedent details from API!")
         raise e
 
 
 if __name__ == "__main__":
-    import os
-    from dotenv import load_dotenv
-    load_dotenv()
-
-    TOMTOM_API_KEY = os.getenv('TOMTOM_API_KEY')
-    usa_bounding_box = "2690124.773468,-15067267.015574,6300857.115604,-7015390.456013"
-
-    print(load_traffic_incident_details(usa_bounding_box, TOMTOM_API_KEY))
+    api_key = BaseHook.get_connection("tomtom_api").password
+    bounding_box = Variable.get("usa_bounding_box")
+    load_traffic_incident_details(bounding_box, api_key)
