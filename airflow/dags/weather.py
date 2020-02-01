87e69bf123909d3ce35b9f7fcd35953b873bf2d5
Jerome Carless <jerome@A8435.lan1>
2020-02-01 15:41:09 -0500

    test

--- airflow/dags/weather.py
+++ airflow/dags/weather.py
@@ -3,7 +3,7 @@ Code that goes along with the Airflow located at:
 http://airflow.readthedocs.org/en/latest/tutorial.html
 """
 
-from weatherOperator import load_forecast
+from weatherOperator.py import load_forecast
 from airflow.exceptions import AirflowException
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.utils import dates
