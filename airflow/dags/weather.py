289393807f50fa77bd9f1847578e449a5107fd58
Jerome Carless <jerome@A8435.lan1>
2020-02-01 15:24:57 -0500

    update path

--- airflow/dags/weather.py
+++ airflow/dags/weather.py
@@ -2,17 +2,19 @@
 Code that goes along with the Airflow located at:
 http://airflow.readthedocs.org/en/latest/tutorial.html
 """
-from datetime import datetime, timedelta
-
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from airflow.models import Variable
-from airflow.utils import dates
-from airflow.hooks.postgres_hook import PostgresHook
-from airflow.exceptions import AirflowException
-
 
 from operators.weather import load_forecast
+from airflow.exceptions import AirflowException
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.utils import dates
+from airflow.models import Variable
+from airflow.operators.python_operator import PythonOperator
+from airflow import DAG
+from datetime import datetime, timedelta
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
 
 default_args = {
     "owner": "airflow",
